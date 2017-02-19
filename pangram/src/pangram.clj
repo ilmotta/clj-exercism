5e82f6a4d4e1170ea4cdf07a4a4787d6b2c33484
ilmotta <icaro.ldm@gmail.com>
2017-02-19 10:25:54 -0300

    Solve pangram problem

---
+++ pangram/src/pangram.clj
@@ -0,0 +1,15 @@
+(ns pangram
+  (:require [clojure.set :as set]
+            [clojure.string :as string]))
+
+(def us-alphabet
+  (set (map char (range (int \a) (inc (int \z))))))
+
+(defn pangram?
+  ([s] (pangram? s us-alphabet))
+  ([s alphabet]
+   (->> (string/lower-case s)
+        (filter alphabet)
+        set
+        (set/difference alphabet)
+        empty?)))
