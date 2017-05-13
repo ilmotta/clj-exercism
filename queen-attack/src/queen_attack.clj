e83da155045223d26ddbd2e8badc6d6c2be227e2
Icaro Motta <imotta@thoughtworks.com>
2017-05-13 16:06:49 -0300

    Solve queen attack

---
+++ queen-attack/src/queen_attack.clj
@@ -0,0 +1,29 @@
+(ns queen-attack
+  (:require [clojure.string :as string]))
+
+(defn can-attack [{[br bc] :b, [wr wc] :w}]
+  (or (= bc wc)
+      (= br wr)
+      (= (Math/abs (- br wr)) (Math/abs (- bc wc)))))
+
+(defn board-place [{:keys [b w]} row col]
+  (let [loc [row col]]
+    (cond
+      (= loc w) :chess/white
+      (= loc b) :chess/black
+      :else :chess/empty)))
+
+(defn board-row [queens row]
+  (map (partial board-place queens row) (range 8)))
+
+(defn board [queens]
+  (map (partial board-row queens) (range 8)))
+
+(defn render-place [place]
+  (get {:chess/white "W" :chess/black "B"} place "_"))
+
+(defn board-string [queens]
+  (-> (board queens)
+      (->> (map #(string/join " " (map render-place %))))
+      (->> (string/join "\n"))
+      (str "\n")))
